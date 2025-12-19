c3573936dee537f5735f84b07cca43afff1ad4ac
labserver <1270517123@qq.com>
2025-12-19 15:23:43 +0800

    savedir

--- family_tour_generation/train_with_ss_rollout.py
+++ family_tour_generation/train_with_ss_rollout.py
@@ -426,7 +426,7 @@ def main():
         train_config=train_config,
         train_loader=train_loader,
         val_loader=val_loader,
-        save_dir='../checkpoints_ss_with_condition',
+        save_dir='../checkpoints_ss_with_condition1',
         eb_strategy='aggressive'  # 可选: 'aggressive', 'conservative'
     )
 
