0c4cb52f26e8edcf4923a042e16004346d5c58d1
labserver <1270517123@qq.com>
2025-12-30 10:50:00 +0800

    less_SS

--- family_tour_generation/train_with_ss_rollout.py
+++ family_tour_generation/train_with_ss_rollout.py
@@ -467,7 +467,7 @@ def main():
         train_config=train_config,
         train_loader=train_loader,
         val_loader=val_loader,
-        save_dir='../checkpoints_ss_with_condition_newple',
+        save_dir='../checkpoints_ss_with_condition_lessSS',
         eb_strategy='aggressive'  # 可选: 'aggressive', 'conservative'
     )
 
