6657a8cd9fa0d108c43e74b7a7ad53a479e6fcbc
cranehh <1270517123@qq.com>
2026-01-09 11:08:05 +0800

    Fix based on old version

--- family_tour_generation/config.py
+++ family_tour_generation/config.py
@@ -45,7 +45,7 @@ class ModelConfig:
     loss_weights: dict = None
 
     # 使用模式专家
-    use_pattern_condition = False
+    use_pattern_condition = True
 
     # ===== 新增：目的地预测配置 =====
     use_destination_prediction: bool = True
